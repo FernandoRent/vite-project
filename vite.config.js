f96d13f7491e660f76be56a9f897fe4d33602437
Fernando Daniel Renteria Saldaña <fernandod2004@hotmail.com>
2025-04-03 19:07:50 -0600

    bd

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   base: '/vite-project/',
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/setupTests.js']
-  }
 })
